7410d5831e4f51bbfcc59b4cea11fcd8a3bc0fb1
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2023-02-20 21:01:58 +0000

    added code for selecting random characters

--- script.js
+++ script.js
@@ -1,102 +1,33 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-
-// Function to prompt user for password options
+// Function to prompt user for password options - when user checks whihch character they want to include in their password 
 function getPasswordOptions() {
 
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
-
+function getRandomSymbol() {
+  const symbols = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+  console.log(symbols[Math.floor(Math.random(symbols) * symbols.length)]);
 }
+getRandomSymbol();
+
+function getRandomNumber() {
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  console.log(numbers[Math.floor(Math.random(numbers) * numbers.length)]);
+}
+getRandomNumber()
+
+function getRandomLower() {
+  const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  console.log(lowerCase[Math.floor(Math.random(lowerCase) * lowerCase.length)]);
+}
+getRandomLower()
+
+function getRandomUpper() {
+  const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  console.log(upperCase[Math.floor(Math.random(upperCase) * upperCase.length)]);
+}
+getRandomUpper()
 
 // Function to generate password with user input
 function generatePassword() {
